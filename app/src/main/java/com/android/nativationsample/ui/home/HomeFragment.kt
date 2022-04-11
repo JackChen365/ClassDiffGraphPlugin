c9fc6189348b243f078824a82928a4281b3fdda9
JackChen <zhenchen@tubi.tv>
2022-04-11 21:41:03 +0800

    add .gitignore and workflow file

--- app/src/main/java/com/android/nativationsample/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/nativationsample/ui/home/HomeFragment.kt
@@ -4,39 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.android.nativationsample.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    // Obtain ViewModel
+    private val viewmodel: LiveDataViewModel by viewModels { LiveDataVMFactory }
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.refreshButton.setOnClickListener {
+            viewmodel.onRefresh()
+        }
+        viewmodel.currentTime.observe(viewLifecycleOwner) { value ->
+            binding.time.text = value?.toString()
+        }
+        viewmodel.currentTimeTransformed.observe(viewLifecycleOwner) { value ->
+            binding.timeTransformed.text = value
+        }
+        viewmodel.currentWeather.observe(viewLifecycleOwner) { value ->
+            binding.currentWeather.text = value
+        }
+        viewmodel.cachedValue.observe(viewLifecycleOwner) { value ->
+            binding.cachedValue.text = value
+        }
     }
 }
